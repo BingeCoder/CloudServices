70bc4fcddbb6bbc3707a78e88cf10b51bbc982ea
Gunjan Srivastava <Techteam@ringdoctors.com>
2021-02-22 16:34:02 -0800

    Migrated api's in index.js

--- src/apis/post/index.js
+++ src/apis/post/index.js
@@ -35,4 +35,44 @@ router.route('/addinterestedskill')
     res.status(200).json(body);
 });
 
+router.route('/adduser')
+.post((req,res) => {
+    var userObject = JSON.parse(JSON.stringify(req.body));
+    userObject.TableName = "user_details";
+    body = await dynamo.put(userObject).promise();
+    res.status(200).json(body);
+    // Adding to Skills Offered Table Not  Required as per DB discussion
+//    var addUserObjectSkillOffered = JSON.parse(JSON.stringify(event.body));
+//    addUserObjectSkillOffered.TableName = TABLE_SKILL_OFFERED;
+//    addUserObjectSkillOffered.Item = {};
+//    addUserObjectSkillOffered.Item.user_name = addUserObject.Item.user_name;
+//    addUserObjectSkillOffered.Item.name = addUserObject.Item.first_name + " " + addUserObject.Item.last_name;
+//    addUserObjectSkillOffered.Item.skills_offered = [];
+//    console.log('skills_offered:', JSON.stringify(addUserObjectSkillOffered));
+//    body = await dynamo.put(addUserObjectSkillOffered).promise();
+    // Adding to Interested skills not required after db discussion
+//    var addUserObjectSkillInterested = JSON.parse(JSON.stringify(event.body));
+//    addUserObjectSkillInterested.TableName = TABLE_SKILL_INTERESTED;
+//    addUserObjectSkillInterested.Item = {};
+//    addUserObjectSkillInterested.Item.user_name = addUserObject.Item.user_name;
+//    addUserObjectSkillInterested.Item.name = addUserObject.Item.first_name + " " + addUserObject.Item.last_name;
+//    addUserObjectSkillInterested.Item.skills_interested = [];
+//    console.log('skills_offered:', JSON.stringify(addUserObjectSkillInterested));
+//    await dynamo.put(addUserObjectSkillInterested).promise();
+});
+
+router.route('/addofferedskill')
+.post((req, res) => {
+    var user_name = req.body.user_name;
+    var params = new Object();
+    params.TableName = req.body.table_name;
+    params.Key = new Object();
+    params.Key.user_name = user_name;
+    params.UpdateExpression = "SET #Y = list_append(#Y,:y)";
+    params.ExpressionAttributeNames = {"#Y": "skills_offered"};
+    params.ExpressionAttributeValues = { ":y":[req.body.skill] };
+    body = await dynamo.update(params).promise();
+    res.status(200).json(body);
+});
+
 module.exports = router;
